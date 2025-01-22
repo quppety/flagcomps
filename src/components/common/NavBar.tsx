3cb7a2a02be1f0107ef7e89c7cd5f1398a1568be
quppety <sashainiesta+github@gmail.com>
2025-01-22 16:40:13 +0000

    vkewgu

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,33 +1,3 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
-import { useCounterControl } from '../../hooks/useCounterControl';
-
 export default function NavBar() {
-  const { counter, handleCounter } = useCounterControl();
-  return (
-    <>
-      <Box bg={'gray.200'} px={4}>
-        <Flex h={16} alignItems="center" justifyContent="center" gap={10}>
-          <Button
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <img src="/home.png" alt="home" height={30} width={30} />
-          </Button>
-          <Button
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <img src={'light.png'} alt="theme" height={30} width={30} />
-          </Button>
-          <Button
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <img src="/score.png" alt="score" height={32} width={32} />
-          </Button>
-          <Button onClick={handleCounter}>{counter}</Button>
-        </Flex>
-      </Box>
-    </>
-  );
+  return <button>BUTTON</button>;
 }
