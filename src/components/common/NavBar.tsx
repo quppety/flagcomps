2bac1022bd9c01beadef72d7884165f15e30213b
quppety <sashainiesta+github@gmail.com>
2025-01-22 17:16:08 +0000

    new comp

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,46 +1,58 @@
 import { Link, Outlet } from 'react-router-dom';
-import { useColorMode, useColorModeValue } from '@chakra-ui/react';
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { useColorMode } from '@chakra-ui/react';
 import { useCounterControl } from '../../hooks/useCounterControl';
+import Button from './Button';
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const { counter, handleCounter } = useCounterControl();
   return (
     <>
-      <Box bg={useColorModeValue('gray.200', 'gray.600')} px={4}>
-        <Flex h={16} alignItems="center" justifyContent="center" gap={10}>
+      <div
+        style={{
+          backgroundColor: colorMode === 'light' ? '#E2E8F0' : '#4A5568',
+          paddingRight: '1rem',
+          paddingLeft: '1rem',
+        }}
+      >
+        <div
+          style={{
+            height: '4rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '2.5rem',
+          }}
+        >
           <Button
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <Link to="/">
-              <img src="/home.png" alt="home" height={30} width={30} />
-            </Link>
-          </Button>
+            content={
+              <Link to="/">
+                <img src="/home.png" alt="home" height={30} width={30} />
+              </Link>
+            }
+          />
           <Button
             onClick={toggleColorMode}
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <img
-              src={colorMode === 'light' ? '/dark.png' : 'light.png'}
-              alt="theme"
-              height={30}
-              width={30}
-            />
-          </Button>
+            content={
+              <img
+                src={colorMode === 'light' ? '/dark.png' : 'light.png'}
+                alt="theme"
+                height={30}
+                width={30}
+              />
+            }
+          />
           <Button
-            backgroundColor={'transparent'}
-            _hover={{ backgroundColor: 'transparent' }}
-          >
-            <Link to="/scores">
-              <img src="/score.png" alt="score" height={32} width={32} />
-            </Link>
-          </Button>
-          <Button onClick={handleCounter}>{counter}</Button>
-        </Flex>
-      </Box>
+            content={
+              <Link to="/scores">
+                <img src="/score.png" alt="score" height={32} width={32} />
+              </Link>
+            }
+          />
+
+          <Button onClick={handleCounter} content={counter} />
+        </div>
+      </div>
       <Outlet />
     </>
   );
