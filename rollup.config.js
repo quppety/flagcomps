0a59b8a0801b08a1975cef71bb2e42d09d803c05
quppety <sashainiesta+github@gmail.com>
2025-01-22 14:42:22 +0000

    setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,18 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.ts', // Entry point
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs', // CommonJS format
-    exports: 'named',
-  },
+  input: 'src/index.ts', // Entry file
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      exports: 'named',
+    },
+  ],
   plugins: [peerDepsExternal(), resolve(), commonjs(), typescript()],
 };
